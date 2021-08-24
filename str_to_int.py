d3b4c3e15ab92bbc7e3900071ca37e536ebec3cc
VladBell <v.kolokolchikoff@yandex.ru>
2021-08-24 12:25:05 +0300

    fix script for CN-10

--- str_to_int.py
+++ str_to_int.py
@@ -1,18 +1,17 @@
 
-def str_to_int(str_o='', k=1):
-    if not str_o:
+def str_to_int(str_obj=''):
+    negative_factor = 1
+    if not str_obj and str_obj is not None:
         return 0
-    if str_o[0] == '-':
-        k, str_o = -1, str_o.replace('-', '', 1)
-    if str_o.isnumeric():
+    elif str_obj is None:
+        raise TypeError
+    if str_obj[0] == '-':
+        negative_factor, str_obj = -1, str_obj.replace('-', '', 1)
+    if str_obj.isnumeric():
         dict_int = {j: i for i, j in enumerate('0123456789')}
-        len_ = len(str_o)
-        int_ = 0
-        for pow_, num in enumerate(str_o, 1):
-            int_ += dict_int[num] * 10 ** (len_ - pow_)
-        return int_ * k
+        len_string, convert_num = len(str_obj), 0
+        for degree, num in enumerate(str_obj, 1):
+            convert_num += dict_int[num] * 10 ** (len_string - degree)
+        return convert_num * negative_factor
     else:
         raise ValueError
-
-
-print(str_to_int())
